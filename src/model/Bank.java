a78731f075ab9a6de7005ee96ca7a563af0948f4
Isabella Ortiz <isabellaortiz5601@gmail.com>
2020-09-22 20:33:25 -0500

    Update

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 import dataStructure.Queue;
 
 public class Bank {
-    private Queue<Client> line1;
-    private Queue<Client> line2;
+    private Queue<Client> queue1;
+    private Queue<Client> queue2;
 
     private ArrayList<CurrentClients> current;
 
     private ArrayList<BankDeserters> deserters;
 
     public Bank() {
-        line1 = new Queue<>();
-        line2 = new Queue<>();
+        queue1 = new Queue<>();
+        queue2 = new Queue<>();
         current = new ArrayList<>();
         deserters = new ArrayList<>();
     }
